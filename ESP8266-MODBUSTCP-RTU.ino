8a6459a3085750078131c7265ee016ac25f5d499
cnnews <46521067+cnnews@users.noreply.github.com>
2023-09-27 15:46:03 +0800

    Create ESP8266-MODBUSTCP-RTU.ino

---
+++ ESP8266-MODBUSTCP-RTU.ino
@@ -0,0 +1,121 @@
+#ifdef ESP8266
+ #include <ESP8266WiFi.h>
+#else //ESP32
+ #include <WiFi.h>
+#endif
+#include <ModbusTCP.h>
+#include <ModbusRTU.h>
+#include <SoftwareSerial.h>
+//SoftwareSerial S(13, 15);
+EspSoftwareSerial::UART S;//这里用到EspSoftwareSerial的软件串口库，定义RX:D7(13),TX:D8(15),
+//#include "StreamBuf.h"
+//#define BSIZE 1024
+//uint8_t buf1[BSIZE];
+//uint8_t buf2[BSIZE];
+//StreamBuf S1(buf1, BSIZE);
+//StreamBuf S2(buf2, BSIZE);
+//DuplexBuf P1(&S1, &S2);
+//DuplexBuf P2(&S2, &S1);
+//ModbusRTU sym;
+
+int DE_RE = 2//RE在modbus库使用而不在串口库中，使用D4(2)
+
+ModbusRTU rtu;
+ModbusTCP tcp;
+
+IPAddress srcIp;
+
+
+uint16_t transRunning = 0;  // Currently executed ModbusTCP transaction
+uint8_t slaveRunning = 0;   // Current request slave
+ 
+bool cbTcpTrans(Modbus::ResultCode event, uint16_t transactionId, void* data) { // Modbus Transaction callback
+  if (event != Modbus::EX_SUCCESS)                  // If transaction got an error
+    Serial.printf("Modbus result: %02X, Mem: %d\n", event, ESP.getFreeHeap());  // Display Modbus error code (222527)
+  if (event == Modbus::EX_TIMEOUT) {    // If Transaction timeout took place
+    tcp.disconnect(tcp.eventSource());          // Close connection
+  }
+  return true;
+}
+
+bool cbRtuTrans(Modbus::ResultCode event, uint16_t transactionId, void* data) {
+    if (event != Modbus::EX_SUCCESS)                  // If transaction got an error
+      Serial.printf("Modbus result: %02X, Mem: %d\n", event, ESP.getFreeHeap());  // Display Modbus error code (222527)
+    return true;
+}
+
+
+// Callback receives raw data 
+Modbus::ResultCode cbTcpRaw(uint8_t* data, uint8_t len, void* custom) {
+  auto src = (Modbus::frame_arg_t*) custom;
+  
+  Serial.print("TCP IP: ");
+  Serial.print(IPAddress(src->ipaddr));
+  Serial.printf(" Fn: %02X, len: %d \n\r", data[0], len);
+
+  if (transRunning) { // Note that we can't process new requests from TCP-side while waiting for responce from RTU-side.
+    tcp.errorResponce(src->ipaddr, (Modbus::FunctionCode)data[0], Modbus::EX_SLAVE_DEVICE_BUSY);
+    return Modbus::EX_SLAVE_DEVICE_BUSY;
+  }
+
+  srcIp = src->ipaddr;
+  
+  slaveRunning = src->slaveId;
+  
+  transRunning = src->transactionId;
+  
+  rtu.rawRequest(slaveRunning, data, len, cbRtuTrans);
+
+  return Modbus::EX_SUCCESS;  
+  
+}
+
+
+// Callback receives raw data from ModbusTCP and sends it on behalf of slave (slaveRunning) to master
+Modbus::ResultCode cbRtuRaw(uint8_t* data, uint8_t len, void* custom) {
+  auto src = (Modbus::frame_arg_t*) custom;
+  tcp.setTransactionId(transRunning); // Set transaction id as per incoming request
+  uint16_t succeed = tcp.rawResponce(srcIp, data, len, slaveRunning);
+  if (!succeed){
+    Serial.print("fail");
+  }
+  Serial.printf("RTU Slave: %d, Fn: %02X, len: %d, ", src->slaveId, data[0], len);
+  Serial.print("Response TCP IP: ");
+  Serial.println(srcIp);
+  
+  transRunning = 0;
+  slaveRunning = 0;
+  return Modbus::EX_PASSTHROUGH;
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin("MSSID", "PASSWORD");//这里根据自己情况改
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+    
+  tcp.server(); // Initialize ModbusTCP to pracess as server
+  tcp.onRaw(cbTcpRaw); // Assign raw data processing callback
+  
+  S.begin(9600, SWSERIAL_8N1, 13, 15 ,false);
+  rtu.begin(&S, DE_RE ,false);  // Specify RE_DE control pin，flash用来反转DE管脚的逻辑，我用的ZLG RSM3485ECHT要求使用低电平发送数据
+  //sym.begin((Stream*)&P2);
+  //sym.slave(1);
+  //sym.addHreg(1, 100);
+  //rtu.begin((Stream*)&P1);  // Specify RE_DE control pin
+  rtu.master(); // Initialize ModbusRTU as master
+  rtu.onRaw(cbRtuRaw); // Assign raw data processing callback
+}
+
+void loop() { 
+  //sym.task();
+  rtu.task();
+  tcp.task();
+  yield();
+}
