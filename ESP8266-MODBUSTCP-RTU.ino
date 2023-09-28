fad49117fab4a5ba32ae703a71f030d6bc3b1aa7
cnnews <46521067+cnnews@users.noreply.github.com>
2023-09-28 10:45:51 +0800

    打印MAC

--- ESP8266-MODBUSTCP-RTU.ino
+++ ESP8266-MODBUSTCP-RTU.ino
@@ -1,8 +1,9 @@
 //
-//我用的硬件是nodeMCU(ESP-12F)+RSM3485ECHT,板载CH340用作下载口和诊断，另外使用CP2102N的RS485数据线连接软串口用作modbus slave的测试端口。
-//实测支持modbus poll多ID同时读modbus slave，但由于系统是同步读写数据故性能要差一些，每秒大概可以处理5次请求。
+//使用的硬件是nodeMCU(ESP-12F)+RSM3485ECHT,板载CH340用作下载口和诊断，另外使用CP2102N的RS485数据线连接软串口用作modbus slave的测试端口。
+//实测支持modbus poll多ID同时读modbus slave，但由于系统是同步读写数据故性能要差一些，每秒大概可以处理5次请求（测试不严谨）。
 //手测支持一个ID同时读最多29个(03)寄存器,超出modbus poll报"transcation id error"，诊断窗口不停打印"TCP IP: 192.168.3.240 Fn: 03, len: 5"
 //然后只能复位ESP8266重连才可以正常运行。----原因在这里：https://github.com/emelianov/modbus-esp8266/issues/177
+//不停打印"TCP IP:...."的原因与从站丢失有关，见"存在问题"一节
 //It's not about the library it's about SoftwareSerial. It's needed to be configured with larger buffer. Something like:
 //SoftwareSerial S1;
 //..
@@ -10,7 +11,10 @@
 //S1.begin(19200, SWSERIAL_8N1, D1, D2, false, 256, 0);
 //..
 //}
-//存在问题：软串口貌似通信质量较差，超过19200的速度有比较多的错误(也可能是USB-串口数据线的原因)；从站报错/丢失以后不能自动重连，需要按RST复位（已解决）
+//存在问题：软串口貌似通信质量较差，超过19200的速度有比较多的错误(也可能是USB-串口数据线的原因)
+//在19200的速率下请求约30000次时才发生一次错误，在38400的速率下大概有6~7%的错误
+//从站报错/丢失以后不能自动重连，需要按RST复位（已解决，删除TCP错误响应部分，与超时后transRunning/transactionId不能复位有关）
+//
 
 #ifdef ESP8266
  #include <ESP8266WiFi.h>
@@ -107,19 +111,24 @@ Modbus::ResultCode cbRtuRaw(uint8_t* data, uint8_t len, void* custom) {
 
 void setup() {
   Serial.begin(115200);
+  Serial.println();
+  delay(3000);
+  Serial.print("MAC address: ");
+  Serial.println(WiFi.macAddress());
   WiFi.begin("SSID", "PASSWORD");//这里根据自己情况改
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.print(".");
   }
   Serial.println("");
-  Serial.println("IP address: ");
+  Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+
     
   tcp.server(); // Initialize ModbusTCP to pracess as server
   tcp.onRaw(cbTcpRaw); // Assign raw data processing callback
   
-  S.begin(9600, SWSERIAL_8N1, 13, 15 ,false ,256 ,0);//串口速率9600,8数据位,1停止位,无校验
+  S.begin(9600, SWSERIAL_8N1, 13, 15 ,false ,256 ,0);//串口速率9600,8数据位,1停止位,无校验,数据缓存256字节
   rtu.begin(&S, DE_RE ,false);  // Specify RE_DE control pin，参数flase用来反转DE管脚的逻辑，我用的RSM3485ECHT要求低电平发送数据
   //sym.begin((Stream*)&P2);
   //sym.slave(1);
